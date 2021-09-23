7780d234b9a0016fc6aaf099a08e95d77183eecc
Nando <fernandoramos.sjc@gmail.com>
2021-09-23 18:19:29 -0300

    Desafios 02

--- Desafios/src/dio/desafios/ARespostaDeTheon.java
+++ Desafios/src/dio/desafios/ARespostaDeTheon.java
@@ -5,17 +5,21 @@ package dio.desafios;
 DESAFIO
 Ramsay: "(...) você vence se conseguir adivinhar quem eu sou e por que estou torturando você."
 
-Theon deve pensar rápido e adivinhar quem é seu algoz! Entretanto, Ramsay já decidiu o que ele irá fazer depois que Theon der sua resposta.
+Theon deve pensar rápido e adivinhar quem é seu algoz! Entretanto, Ramsay já decidiu o que ele irá fazer depois
+que Theon der sua resposta.
 
-Theon pode dizer que seu algoz é alguma dentre N pessoas. Considere que as pessoas são numeradas de 1 a N. Se Theon responder que seu algoz é a pessoa i, Ramsay irá atingi-lo Ti vezes.
+Theon pode dizer que seu algoz é alguma dentre N pessoas. Considere que as pessoas são numeradas de 1 a N.
+Se Theon responder que seu algoz é a pessoa i, Ramsay irá atingi-lo Ti vezes.
 
-Sua tarefa é ajudar Theon a determinar qual deve ser sua resposta de forma a minimizar o número de vezes que ele será atingido.
+Sua tarefa é ajudar Theon a determinar qual deve ser sua resposta de forma a minimizar o número de vezes
+que ele será atingido.
 
 ENTRADA
 A primeira linha contém um inteiro N (1 ≤ N ≤ 100). A segunda linha contém N inteiros T1, T2, ..., TN (0 ≤ Ti ≤ 20).
 
 SAÍDA
-Imprima uma linha contendo o número da pessoa que Theon deve dizer ser seu algoz. Se existe mais de uma resposta possível, imprima a menor.
+Imprima uma linha contendo o número da pessoa que Theon deve dizer ser seu algoz.
+Se existe mais de uma resposta possível, imprima a menor.
 
  */
 
